0f1d0ba5fce0b8307b2d41536bc4f619918d861d
Darvin Muralitharan <muralitharan.darvin@gmail.com>
2022-09-25 00:17:02 -0400

    E07 Submission

--- E07 - Gimbal Removal/AppClassControls.cpp
+++ E07 - Gimbal Removal/AppClassControls.cpp
@@ -106,24 +106,18 @@ void Application::ProcessKeyboard(void)
 #pragma endregion
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::X))
 	{
-		if (fMultiplier)
-			m_v3Rotation.x -= 1.0f;
-		else
-			m_v3Rotation.x += 1.0f;
+		quaternion quat = glm::angleAxis(0.01f, vector3(1.0f, 0.0f, 0.0f));
+		m_qOrientation = m_qOrientation * quat;
 	}
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Y))
 	{
-		if (fMultiplier)
-			m_v3Rotation.y -= 1.0f;
-		else
-			m_v3Rotation.y += 1.0f;
+		quaternion quat = glm::angleAxis(0.01f, vector3(0.0f, 1.0f, 0.0f));
+		m_qOrientation = m_qOrientation * quat;
 	}
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Z))
 	{
-		if (fMultiplier)
-			m_v3Rotation.z -= 1.0f;
-		else
-			m_v3Rotation.z += 1.0f;
+		quaternion quat = glm::angleAxis(0.01f, vector3(0.0f, 0.0f, 1.0f));
+		m_qOrientation = m_qOrientation * quat;
 	}
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::R))
 	{
